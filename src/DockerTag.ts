c8a1ef5527e5522e5ff35fcd06cfb79c4e04e2c7
Jeff Stephens <jefftheman45@gmail.com>
2020-12-28 14:32:34 -0800

    Implement Docker Tag metadata fetch

---
+++ src/DockerTag.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { DateTime } from "luxon";
+
+export class DockerTag {
+  registry: string;
+  repository: string;
+  tag: string;
+
+  digest: string;
+  dateCreated: DateTime;
+
+  constructor(registry: string, repository: string, tag: string) {
+    this.registry = registry;
+    this.repository = repository;
+    this.tag = tag;
+  }
+
+  /**
+   * Fetch object properties from Docker registry
+   */
+  async enrich() {
+    console.debug(`enriching ${this.repository}:${this.tag}...`);
+
+    this.digest = await this.getDigest();
+    this.dateCreated = await this.getCreateDate();
+    return;
+  }
+
+  private async getDigest(): Promise<string> {
+    const response = await axios.head(
+      `https://${this.registry}/v2/${this.repository}/manifests/${this.tag}`,
+      {
+        auth: {
+          username: process.env.REGISTRY_USERNAME!,
+          password: process.env.REGISTRY_PASSWORD!,
+        },
+        headers: {
+          Accept: "application/vnd.docker.distribution.manifest.v2+json",
+        },
+      }
+    );
+
+    return response.headers["docker-content-digest"];
+  }
+
+  private async getCreateDate(): Promise<DateTime> {
+    // V2 API doesn't have create date, but V1 does :shrug:
+    const response = await axios.get(
+      `https://${this.registry}/v2/${this.repository}/manifests/${this.tag}`,
+      {
+        auth: {
+          username: process.env.REGISTRY_USERNAME!,
+          password: process.env.REGISTRY_PASSWORD!,
+        },
+      }
+    );
+
+    const topLayer = JSON.parse(response.data.history[0].v1Compatibility);
+    return DateTime.fromISO(topLayer.created);
+  }
+}
