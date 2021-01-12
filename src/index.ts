814c6cb2962518218ade6786d5dcf40f99348297
Jeff Stephens <jefftheman45@gmail.com>
2021-01-11 21:56:02 -0800

    Execute policies in order, synchronously

--- src/index.ts
+++ src/index.ts
@@ -14,71 +14,72 @@ const main = async () => {
     }
 
     const config = loadConfig();
-    await Promise.all(
-      config.policies.map(async (policy) => {
-        if (!policy.repository) {
-          throw new Error("Expected repository in policy but didn't find it");
-        }
 
-        console.log(
-          `Checking repository ${policy.repository} (maxTags ${policy.maxTags})...`
+    for (let i = 0; i < config.policies.length; i++) {
+      const policy = config.policies[i];
+
+      if (!policy.repository) {
+        throw new Error("Expected repository in policy but didn't find it");
+      }
+
+      console.log(
+        `=== Checking repository ${policy.repository} (maxTags ${policy.maxTags})...`
+      );
+
+      // get list of tags
+      let tags: Array<string> = [];
+      try {
+        const response = await axios.get(
+          `https://${config.registry}/v2/${policy.repository}/tags/list`,
+          {
+            auth: {
+              username: process.env.REGISTRY_USERNAME!,
+              password: process.env.REGISTRY_PASSWORD!,
+            },
+            headers: {
+              Accept: "application/vnd.docker.distribution.manifest.v2+json",
+            },
+          }
         );
 
-        // get list of tags
-        let tags: Array<string> = [];
-        try {
-          const response = await axios.get(
-            `https://${config.registry}/v2/${policy.repository}/tags/list`,
-            {
-              auth: {
-                username: process.env.REGISTRY_USERNAME!,
-                password: process.env.REGISTRY_PASSWORD!,
-              },
-              headers: {
-                Accept: "application/vnd.docker.distribution.manifest.v2+json",
-              },
-            }
+        const { data } = response;
+        data.tags.forEach((tag: string) => {
+          tags.push(tag);
+        });
+      } catch (err) {
+        if (err.response) {
+          const firstError = err.response.data.errors.pop();
+          throw new Error(
+            `${firstError.code} while listing tags: ${firstError.message}`
+          );
+        } else {
+          throw err;
+        }
+      }
+
+      console.log(`Found ${tags.length} tags`);
+
+      // enrich with manifest data
+      let enrichedTags: Array<DockerTag> = tags.map(
+        (tag) => new DockerTag(config.registry, policy.repository, tag)
+      );
+      await Promise.all(enrichedTags.map((tag) => tag.enrich()));
+
+      if (policy.maxTags) {
+        const tagsToDelete = getExpiredTags(enrichedTags, policy.maxTags);
+
+        if (tagsToDelete.length > 0) {
+          console.log(
+            `More tags present than policy allows; scheduling the following for deletion:`,
+            tagsToDelete.map((t) => `${t.tag} (${t.digest})`)
           );
 
-          const { data } = response;
-          data.tags.forEach((tag: string) => {
-            tags.push(tag);
-          });
-        } catch (err) {
-          if (err.response) {
-            const firstError = err.response.data.errors.pop();
-            throw new Error(
-              `${firstError.code} while listing tags: ${firstError.message}`
-            );
-          } else {
-            throw err;
-          }
+          await Promise.all(tagsToDelete.map((tag) => tag.delete()));
+        } else {
+          console.log(`Tag count is under policy limit; nothing to do!`);
         }
-
-        console.log(`Found ${tags.length} tags`);
-
-        // enrich with manifest data
-        let enrichedTags: Array<DockerTag> = tags.map(
-          (tag) => new DockerTag(config.registry, policy.repository, tag)
-        );
-        await Promise.all(enrichedTags.map((tag) => tag.enrich()));
-
-        if (policy.maxTags) {
-          const tagsToDelete = getExpiredTags(enrichedTags, policy.maxTags);
-
-          if (tagsToDelete.length > 0) {
-            console.log(
-              `More tags present than policy allows; scheduling the following for deletion:`,
-              tagsToDelete.map((t) => `${t.tag} (${t.digest})`)
-            );
-
-            await Promise.all(tagsToDelete.map((tag) => tag.delete()));
-          } else {
-            console.log(`Tag count is under policy limit; nothing to do!`);
-          }
-        }
-      })
-    );
+      }
+    }
   } catch (err) {
     console.error(`Error: ${err.message}`);
   }
